970de57d96dd1bb8a7741fff3e3465c60384def1
Mike Homolak <mhomolak@yahoo.com>
2016-07-03 16:18:24 -0700

    blah

--- knexfile.js
+++ knexfile.js
@@ -5,19 +5,19 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
-  },
+    connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
+    },
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
-  },
+    connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
-  }
+    }
+  },
 
 };

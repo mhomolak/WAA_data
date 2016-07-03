90d91c827c197554329b69346301470d5a9507fb
Mike Homolak <mhomolak@yahoo.com>
2016-07-03 16:15:14 -0700

    dotenv shit

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,17 @@
-// Update with your config settings.
 require('dotenv').config();
 
 
 module.exports = {
 
   development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL
   },

999be9c8ef42a59732957941dfc1603bdd3f75ce
Mike Homolak <mhomolak@yahoo.com>
2016-07-06 22:31:33 -0700

    minor styling and text change

--- src/layout/landing.js
+++ src/layout/landing.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Landing = () => (
   <div>
-    <h1>Home</h1>
+    <h1 className="landingHead">MLB Analytics</h1>
     <img className="bgImage" src="https://upload.wikimedia.org/wikipedia/commons/5/59/Baseball_diamond_marines.jpg"/>
-    <p className="homeText">Lorem ipsum dolor sit amet, consectetur adipisicing
-    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p className="homeText">In recent years, the idea of using statistical analysis
+    to find undervalued players has become more and more prevalent.  Former saught after
+    stats such as home runs and strikeouts are being replaced by OBP (On base Percentage) and
+    WHIP (Walks and hits per inning pitched). MLB Analytics looks into the spending
+    of the past twenty years to identify which teams have found true value and the
+    outcomes of those seasons.</p>
   </div>
 )
 export default Landing
